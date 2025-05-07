ee7371d5666d8b231cb908a5110827451c01c1ef
Gary Bortosky <garyb432@users.noreply.github.com>
2025-05-07 17:07:33 -0500

    add workflow

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import gb from "eslint-plugin-gb";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js, gb },
+    extends: ["js/recommended", "gb/flat/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
