a9576ba4c7f57082dee16fab7a059de6e3400d75
Gary Bortosky <garyb432@users.noreply.github.com>
2025-05-10 14:43:57 -0500

    test: refactor HandleClaims tests to use beforeAll and update test cases

--- src/wikibase/handle-claims.spec.ts
+++ src/wikibase/handle-claims.spec.ts
@@ -1,20 +1,31 @@
-import { describe, expect, test } from "vitest";
+import { beforeAll, beforeEach, describe, expect, test } from "vitest";
 import { handlePropertyClaims } from "./handle-claims";
 import { type PersonInfo } from "./types";
 
-describe("HandleClaims", () => {
-  test.skip("handlePropertyClaims", async () => {
-    const fakey: PersonInfo = {
-      id: "Q34",
-      name: "Bob",
-      birthYear: 0,
-      occupations: [],
-      schools: [],
-      fetchDate: expect.any(Date()),
-      sources: [],
-    };
-    await handlePropertyClaims({ type: "item", id: "Q34", claims: {} }, fakey);
+describe("HandleClaims", async () => {
+  const fakey: PersonInfo = {
+    id: "Q0",
+    name: "Bob",
+    birthYear: 0,
+    occupations: [],
+    schools: [],
+    fetchDate: expect.any(Date()),
+    sources: [],
+  };
 
-    expect(fakey).toBeUndefined();
+  beforeAll(async () => {
+    await handlePropertyClaims({ type: "item", id: "Q0", claims: {} }, fakey);
   });
+  test("aliases", () => { expect(fakey.aliases).toBeUndefined(); });
+  test("awards", () => { expect(fakey.awards).toBeUndefined(); });
+  test("children", () => { expect(fakey.children).toBeUndefined(); });
+  test("employers", () => { expect(fakey.employers).toBeUndefined(); });
+  test("fieldOfWork", () => { expect(fakey.fieldOfWork).toEqual([ 'Anonymity', 'Unit Testing' ]); });
+  test("nationality", () => { expect(fakey.nationality).toBeUndefined(); });
+  test("notableWorks", () => { expect(fakey.notableWorks).toBeUndefined(); });
+  test("occupations", () => { expect(fakey.occupations).toEqual([]); });
+  test("parents", () => { expect(fakey.parents).toBeUndefined(); });
+  test("schools", () => { expect(fakey.schools).toEqual([]); });
+  test("sources", () => { expect(fakey.sources).toEqual([]); });
+  test("spouses", () => { expect(fakey.spouses).toBeUndefined(); });
 });
