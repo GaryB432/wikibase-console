f6dc097a4351f7b7a33bc1cecc2067255b5579c9
Gary Bortosky <garyb432@users.noreply.github.com>
2025-05-15 10:01:21 -0500

    refactor: update search functionality to use prompts for selecting a person and improve handling of search results

--- src/main.ts
+++ src/main.ts
@@ -13,28 +13,89 @@ import { handlePerson } from "./wikibase/handle-entity.js";
 //   P.NOTABLE_WORK,
 // ];
 
-const search = "einstein";
+import prompts from "prompts";
 
-wikibaseService.searchForHumans(search).then(
-  (q) => {
-    const questions = q.map((e) => {
-      const { id, label } = e;
-      return { id, label };
-    });
-    if (questions.length === 1) {
-      handlePerson((questions[0]!.id as EntityId)).then(
-        (p) => {
-          console.log(JSON.stringify(p, undefined, 4));
-        },
-        (e) => {
-          console.log("nope", e);
-        },
-      );
+const search = "ludwig beethoven";
+
+// wikibaseService.searchForHumans(search).then(
+//   (found) => {
+//     let id: EntityId | null = null;
+//     // const qs = found.map((e) => {
+//     //   const { id, label, description } = e;
+//     //   return { id, label, description };
+//     // });
+
+//     // const questions = qs.map(q=> ({...q, type:'select', message: 'Pick a Human', choices: []}));
+
+//     if (found.length > 0) {
+//       if (found.length === 1) {
+//         id = found[0]!.id as EntityId;
+//       } else {
+//         prompts({
+//           type: "select",
+//           name: "id",
+//           message: "Who's your guy",
+//           choices: found.map((f) => ({
+//             title: f.description ?? "hmm",
+//             value: f.id,
+//           })),
+//         }).then(
+//           (r) => {
+//             console.log(r.id, "is your guy");
+//             id = r.id;
+//           },
+//           (e) => {
+//             console.error("bad things", e);
+//           }
+//         );
+//         if (id) {
+//           handlePerson(id).then(
+//             (p) => {
+//               console.log(JSON.stringify(p, undefined, 4));
+//             },
+//             (e) => {
+//               console.log("nope", e);
+//             }
+//           );
+//         }
+//       }
+
+//       // // TODO handle
+//       // // questionsForFake.forEach((q) => console.log(q.label, q.description, q.id));
+//       // console.error(`${questionsForFake.length} ambiguity unhandled for now`);
+//     }
+//   },
+//   () => {}
+// );
+
+async function main(): Promise<void> {
+  let subjectId: EntityId | null = null;
+  const results = await wikibaseService.searchForHumans(search);
+
+  if (results.length > 0) {
+    if (results.length === 1) {
+      subjectId = results[0]!.id as EntityId;
     } else {
-      // TODO handle 
-      questions.forEach((q) => console.log(q.label, q.id));
-      console.error(`${questions.length} ambuguity unhandled for now`);
+      const promptedPerson = await prompts({
+        type: "select",
+        name: "id",
+        message: "Select the appropriate person from Wikidata",
+        choices: results.map((res) => ({
+          title: res.description ?? res.label,
+          value: res.id,
+        })),
+      });
+      subjectId = promptedPerson.id;
     }
-  },
+  }
+
+  if (subjectId) {
+    const p = await handlePerson(subjectId);
+    console.log(JSON.stringify(p, undefined, 4));
+  }
+}
+
+main().then(
+  () => {},
   () => {},
 );
